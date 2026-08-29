a327896a887a18ab79d7b9ff4204fed6933447fe
Suhan-sp <Suhansp05@gmail.com>
2026-08-29 15:01:22 +0530

    docker files added

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import path from "path";
+import fs from "fs";
 
 import { clerkMiddleware } from "@clerk/express";
 
@@ -15,6 +17,8 @@ const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const PORT = process.env.PORT || 3000;
 
+const publicDir = path.join(process.cwd(), "public");
+
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
@@ -22,6 +26,14 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
     res.status(200).json({ok: true});
 });
+
+if(fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+
+    app.get("/{*any}", (req, res,next) => {
+        res.sendFile(path.join(publicDir, "index.html"),(err) => next(err));
+    });
+}
 
 async function startServer() {
     try {
