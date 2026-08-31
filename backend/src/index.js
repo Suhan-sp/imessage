83d2d00c77a6448461278ef8f12ebf90f746ec0c
Suhan-sp <Suhansp05@gmail.com>
2026-08-31 11:41:20 +0530

    Addclerk webhook handler to process user event and update database

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,9 @@ import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/User.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/crons.js";
+
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
@@ -19,9 +22,13 @@ const PORT = process.env.PORT || 3000;
 
 const publicDir = path.join(process.cwd(), "public");
 
+//its important that u dont parse the webhook event data it should be in raw formats
+app.use("/api/webhooks/clerk",express.raw({type: "application/json"}),clerkWebhook);
+
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
+
 
 app.get("/health", (req, res) => {
     res.status(200).json({ok: true});
@@ -42,6 +49,10 @@ async function startServer() {
         app.listen(PORT, () => {
             console.log("MongoDB connected successfully");
             console.log("Server is running on port", PORT);
+
+            if(process.env.NODE_ENV === "production") {
+                job.start()
+            }
         });
     } catch (error) {
         console.error("Failed to start server:", error);
