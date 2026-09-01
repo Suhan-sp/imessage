e81c2ac7e6bb4ba954626f9a8208596aefd1676c
Suhan-sp <Suhansp05@gmail.com>
2026-09-01 15:37:57 +0530

    Added auth file, sounds, wallpapers,updated index.cc to integrate tailwind css and heroui styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Button } from '@heroui/react';
+import { ThemeProvider } from './context/ThemeContext';
+import { WallpaperProvider } from './context/WallpaperContext';
+import ChatPage from './pages/styles/ChatPage';
+import AuthPage from './pages/styles/AuthPage';
+import { Navigate, Routes, Route } from 'react-router';
+import { useAuth } from '@clerk/react';
 
 function App() {
-  return (
-    <div>
-      <h1>My app</h1>
 
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode = "modal" />
-          <SignUpButton mode = "modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
-  )
+  const { isSignedIn, isLoaded } = useAuth();
+
+   return (
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
